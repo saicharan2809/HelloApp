332c91a296f560a3b5db93d144df5e5dbbdcf9a7
saicharan2809 <chaganamsaicharan@gmail.com>
2026-03-11 21:36:17 +0530

    ADDED UC2:HELLO APP

--- HelloApp1.java
+++ HelloApp1.java
@@ -1,6 +1,7 @@
 public class HelloApp1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 
 }
