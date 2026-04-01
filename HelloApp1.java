2261e422dafc43282a9c6fe9a94ef023e2e0725e
saicharan2809 <chaganamsaicharan@gmail.com>
2026-04-02 00:34:16 +0530

    Coded UC4:multiple command line arguments

--- HelloApp1.java
+++ HelloApp1.java
@@ -1,10 +1,13 @@
 public class HelloApp1 {
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length > 0) {
-            name = args[0];
+        StringBuilder nameBuilder = new StringBuilder("World");
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+            if (i<args.length-1) {
+                nameBuilder.append(" ");
+            }
         }
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
     }
 
 }
